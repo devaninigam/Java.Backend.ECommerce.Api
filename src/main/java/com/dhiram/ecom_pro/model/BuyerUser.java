64bd01b9233fc3cbe57f50ef684d9a6bcd8abc3a
devaninigam <nigamdevani2029@gmail.com>
2025-06-26 16:40:18 +0530

    feat: Add cart management features including update, delete, and retrieval of user carts

--- src/main/java/com/dhiram/ecom_pro/model/BuyerUser.java
+++ src/main/java/com/dhiram/ecom_pro/model/BuyerUser.java
@@ -1,18 +1,32 @@
 package com.dhiram.ecom_pro.model;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
-
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
@@ -65,7 +79,7 @@ public class BuyerUser {
     @Column(nullable = false)
     private String phone;
 
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(
             name = "buyer_product_history",
             joinColumns = @JoinColumn(name = "buyer_id")
--- src/main/java/com/dhiram/ecom_pro/service/BuyerUserProductService.java
+++ src/main/java/com/dhiram/ecom_pro/service/BuyerUserProductService.java
@@ -2,17 +2,25 @@ package com.dhiram.ecom_pro.service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.dhiram.ecom_pro.dto.DeleteCartsRequest;
 import com.dhiram.ecom_pro.dto.PostAddCartsRequest;
+import com.dhiram.ecom_pro.model.BuyerUser;
+import com.dhiram.ecom_pro.model.BuyerUser.ProductHistoryEntry;
 import com.dhiram.ecom_pro.model.ProductModel;
+import com.dhiram.ecom_pro.repo.BuyerRepo;
 import com.dhiram.ecom_pro.repo.ProductRepo;
+
 
 @Service
 public class BuyerUserProductService {
@@ -20,23 +28,15 @@ public class BuyerUserProductService {
     @Autowired
     private ProductRepo productRepo;
 
+    @Autowired
+    private BuyerRepo buyerRepo;
+
     public ResponseEntity<?> getAllProducts() {
         List<ProductModel> products = productRepo.findAll();
         List<Map<String, Object>> productList = new ArrayList<>();
 
         for (ProductModel product : products) {
-            Map<String, Object> productData = new HashMap<>();
-            productData.put("id", product.getId());
-            productData.put("name", product.getName());
-            productData.put("description", product.getDescription());
-            productData.put("brand", product.getBrand());
-            productData.put("price", product.getPrice());
-            productData.put("category", product.getCategory());
-            productData.put("releaseDate", product.getReleaseDate());
-            productData.put("available", product.isAvailable());
-            productData.put("quantity", product.getQuantity());
-            productData.put("imageData", product.getImageData());
-            productList.add(productData);
+            productList.add(convertProductToMap(product));
         }
 
         if (productList.isEmpty()) {
@@ -56,12 +56,207 @@ public class BuyerUserProductService {
 
     public ResponseEntity<?> postAddCarts(PostAddCartsRequest postAddCartsRequest) {
         ProductModel product = productRepo.findById(postAddCartsRequest.getProductId()).orElse(null);
+        BuyerUser buyerUser = buyerRepo.findById(postAddCartsRequest.getBuyerUserId()).orElse(null);
         if (product == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of(
                     "message", "Product not found",
                     "status", "error"
             ));
         }
-        return null;
+
+        if (buyerUser == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of(
+                    "message", "User not found",
+                    "status", "error"
+            ));
+        }
+
+        for (ProductHistoryEntry productHistory : buyerUser.getProductHistory()) {
+            if (productHistory.getProductId().equals(postAddCartsRequest.getProductId())) {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of(
+                        "message", "Product already exists in cart",
+                        "status", "error",
+                        "details", "Product with ID " + postAddCartsRequest.getProductId() + " is already in the user's cart"
+                ));
+            }
+        }
+
+        if (!product.isAvailable()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of(
+                    "message", "Product is not available",
+                    "status", "error"
+            ));
+        } else if (product.getQuantity() < postAddCartsRequest.getQuantity()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of(
+                    "message", "Product quantity is not enough, product has only " + product.getQuantity() + " quantity",
+                    "status", "error"
+            ));
+        }
+
+        buyerUser.getProductHistory().add(new ProductHistoryEntry(
+                postAddCartsRequest.getProductId(),
+                postAddCartsRequest.getQuantity()
+        ));
+
+        buyerRepo.save(buyerUser);
+        return ResponseEntity.ok(Map.of(
+                "message", "Product added to cart successfully",
+                "status", "success"
+        ));
+
     }
+
+    public ResponseEntity<?> putUpdateCarts(PostAddCartsRequest postAddCartsRequest) {
+        ProductModel product = productRepo.findById(postAddCartsRequest.getProductId()).orElse(null);
+        BuyerUser buyerUser = buyerRepo.findById(postAddCartsRequest.getBuyerUserId()).orElse(null);
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of(
+                    "message", "Product not found",
+                    "status", "error"
+            ));
+        }
+
+        if (buyerUser == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of(
+                    "message", "User not found",
+                    "status", "error"
+            ));
+        }
+
+        int cartIndex = -1;
+        int i = 0;
+
+        for (ProductHistoryEntry productHistory : buyerUser.getProductHistory()) {
+            if (productHistory.getProductId().equals(postAddCartsRequest.getProductId())) {
+                cartIndex = i;
+                break;
+            }
+            i++;
+        }
+
+        if (!product.isAvailable()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of(
+                    "message", "Cannot update cart - product is currently unavailable",
+                    "status", "error"
+            ));
+        } else if (product.getQuantity() < postAddCartsRequest.getQuantity()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of(
+                    "status", "error",
+                    "code", "INSUFFICIENT_STOCK",
+                    "message", "Insufficient product quantity",
+                    "available", product.getQuantity(),
+                    "requested", postAddCartsRequest.getQuantity(),
+                    "productId", product.getId()
+            ));
+        }
+
+        ProductHistoryEntry updatedEntry = new ProductHistoryEntry(
+                postAddCartsRequest.getProductId(),
+                postAddCartsRequest.getQuantity()
+        );
+
+        buyerUser.getProductHistory().set(cartIndex, updatedEntry);
+        buyerRepo.save(buyerUser);
+        return ResponseEntity.ok(Map.of(
+                "status", "success",
+                "message", "Cart updated successfully",
+                "data", Map.of(
+                        "productId", postAddCartsRequest.getProductId(),
+                        "newQuantity", postAddCartsRequest.getQuantity()
+                )
+        ));
+
+    }
+
+    public ResponseEntity<?> getAllCarts(UUID buyerId) {
+        BuyerUser buyerUser = buyerRepo.findById(buyerId).orElse(null);
+
+        if (buyerUser == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of(
+                    "message", "User not found",
+                    "status", "error"
+            ));
+        }
+
+        if (buyerUser.getProductHistory().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of(
+                    "message", "No cart history found for this user",
+                    "status", "error",
+                    "details", "The user exists, but no products have been added to the cart yet"
+            ));
+        }
+
+        List<Map<String, Object>> productHistoryList = new ArrayList<>();
+
+        for (ProductHistoryEntry productHistory : buyerUser.getProductHistory()) {
+            ProductModel product = productRepo.findById(productHistory.getProductId()).orElse(null);
+            Map<String, Object> productHistoryData = new HashMap<>();
+            productHistoryData.put("product", convertProductToMap(product));
+            productHistoryData.put("orderQuntity", productHistory.getQuantity());
+            productHistoryList.add(productHistoryData);
+        }
+
+        return ResponseEntity.ok().body(Map.of(
+                "data", productHistoryList,
+                "message", "Cart retrieved successfully",
+                "status", "success",
+                "count", productHistoryList.size()
+        ));
+    }
+
+    private Map<String, Object> convertProductToMap(ProductModel product) {
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("id", product.getId());
+        map.put("name", product.getName());
+        map.put("description", product.getDescription());
+        map.put("brand", product.getBrand());
+        map.put("price", product.getPrice());
+        map.put("category", product.getCategory());
+        map.put("releaseDate", product.getReleaseDate());
+        map.put("available", product.isAvailable());
+        map.put("quantity", product.getQuantity());
+        map.put("imageData", product.getImageData());
+        return map;
+    }
+
+  public ResponseEntity<?> deleteCarts(DeleteCartsRequest deleteCartsRequest) {
+    BuyerUser buyerUser = buyerRepo.findById(deleteCartsRequest.getBuyerUserId())
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+    int index = -1;
+    for (int i = 0; i < buyerUser.getProductHistory().size(); i++) {
+        if (buyerUser.getProductHistory().get(i).getProductId().equals(deleteCartsRequest.getProductId())) {
+            index = i;
+            break;
+        }
+    }
+
+    // 3. Remove product from cart
+    if (index == -1) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of(
+            "status", "error",
+            "code", "PRODUCT_NOT_IN_CART",
+            "message", "Product not found in user's cart",
+            "details", Map.of(
+                "productId", deleteCartsRequest.getProductId(),
+                "userId", deleteCartsRequest.getBuyerUserId()
+            )
+        ));
+    }
+
+    // 4. Remove product from cart
+    ProductHistoryEntry removedItem = buyerUser.getProductHistory().remove(index);
+    buyerRepo.save(buyerUser);
+
+    return ResponseEntity.ok(Map.of(
+        "status", "success",
+        "code", "ITEM_REMOVED",
+        "message", "Product successfully removed from cart",
+        "data", Map.of(
+            "removedProductId", removedItem.getProductId(),
+            "removedQuantity", removedItem.getQuantity(),
+            "remainingItems", buyerUser.getProductHistory().size()
+        )
+    ));
+}
 }
